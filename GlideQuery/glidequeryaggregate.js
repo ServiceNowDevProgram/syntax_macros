feab121ca30585b09eae10ae5e2edd27878d4572
alikuttyka <47845869+alikuttyka@users.noreply.github.com>
2021-10-04 00:11:56 +0530

    Update glidequeryaggregate.js

--- GlideQuery/glidequeryaggregate.js
+++ GlideQuery/glidequeryaggregate.js
@@ -2,8 +2,8 @@ var aggregateQueryObj = new global.GlideQuery('table_name')
     .where('field_name', 'field_value')
     .groupBy('group_by_field')
     .aggregate('aggregate_type', 'aggregate_field_name')
-    .having('aggregate_type', 'aggregate_field_name', 'operator', 'number_value')
+    .having('aggregate_type', 'aggregate_field_name', 'operator', number_value)
     .select()
-    .forEach(function(g) {
-        //gs.info(JSON.stringify(aggregateQueryObj, null, 2));
+    .forEach(function(row) {
+        //gs.info(JSON.stringify(row, null, 2));
     });
