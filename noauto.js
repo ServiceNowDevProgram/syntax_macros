f4289a80dd209fe55dd927c7d6f45cf0602e2af6
Ingimar Robertsson <iar42@users.noreply.github.com>
2021-10-04 18:34:34 +0000

    Disable business rules and sys field updates
    Useful for fix or background scripts before GlideRecord update() to prevent business rules execution and timestamp updates when bulk changing records.

---
+++ noauto.js
@@ -0,0 +1,2 @@
+gr.autoSysFields(false);  // Do not update sys_updated_by, sys_updated_on, sys_mod_count, sys_created_by, and sys_created_on
+gr.setWorkflow(false);    // Do not run any other business rules
