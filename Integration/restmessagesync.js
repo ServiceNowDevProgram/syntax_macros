512631cd56e7839494598ba8ad71e58cfcc3367a
Alikutty Abdulrazak <47845869+alikuttyka@users.noreply.github.com>
2021-10-11 21:27:34 +0530

    Macros for making rest and soap message sync calls (#28)
    * Syntax macro for GlideRecord encoded query
    
    * Syntax macro for inserting record
    
    * Update and rename gliderecordinsert.js to gliderecordupdatequery.js
    
    * syntax macro to update a record accessed via get
    
    * syntax macro to update a record accessed via encode query
    
    * syntax macro to delete record accessed via encoded query
    
    * syntax macro to delete a record accessed via get
    
    * Glide query template for inserts
    
    * Glide query template for updating multiple records
    
    * Removing it from list
    
    * Removing it from the list
    
    * Adding GlideRecord folder
    
    * Adding to GlideRecord folder
    
    * Adding to GlideRecord folder
    
    * Adding to GlideRecord folder
    
    * Moving to GlideRecord folder
    
    * Template for inserting records via GlideQuery
    
    * Update multiple records via GlideQuery
    
    * Template to perform aggregate on GlideQuery
    
    * Rename glidequery.aggregate.js to glidequeryaggregate.js
    
    * Update glidequeryaggregate.js
    
    * Template for selecting one record via glidequery
    
    * Template for selecting multiple records via glidequery
    
    * Updating a single record via glide query
    
    * Template for deleting records via glidequery
    
    * Create restmessagesync.js
    
    * Create soapmessagesync.js

---
+++ Integration/restmessagesync.js
@@ -0,0 +1,22 @@
+try { 
+ var r = new sn_ws.RESTMessageV2('message_name', 'method_name');
+
+//override authentication profile 
+//authentication type ='basic'/ 'oauth2'
+//r.setAuthenticationProfile(authentication type, profile name);
+
+//set a MID server name if one wants to run the message on MID
+//r.setMIDServer('MY_MID_SERVER');
+
+ r.setStringParameter("variable_name1", "variable_value1");
+ r.setStringParameter("variable_name2", "variable_value2");
+ r.setStringParameter("variable_name3", "variable_value3");
+ r.setStringParameterNoEscape("variable_name4", "variable_value4");
+  
+ var response = r.execute();
+ var responseBody = response.getBody();
+ var httpStatus = response.getStatusCode();
+}
+catch(ex) {
+ var message = ex.message;
+}
