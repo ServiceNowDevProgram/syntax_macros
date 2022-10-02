f4f7d410500048283772584ecbb2d1b98fc833a5
Seth Rose <10383809+b1naryst0rm@users.noreply.github.com>
2022-10-02 13:57:39 -0500

    Adding Loop Macros

---
+++ General/for.js
@@ -0,0 +1,3 @@
+for (var i=0; i< myArray.length; i++) {
+  //myArray[i];
+}
---
+++ General/foreach.js
@@ -0,0 +1,6 @@
+for (property in event) {
+    //code block
+    if (condition) { //optional
+        continue; //skip the rest of the code and go to next property in the event
+    }
+}
