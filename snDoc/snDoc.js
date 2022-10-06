9758151095b16bc3a3727fa5784ca1313bccad0f
Ryan Litwiller <litwi1rm@gmail.com>
2022-10-05 23:01:40 -0400

    snDoc - Script Include method comments (#59)

---
+++ snDoc/snDoc.js
@@ -0,0 +1,8 @@
+/**SNDOC
+@name nameOfMethod
+@description description of method
+@param  {param type i.e. string} [paramName] - description of param
+@returns {return type i.e. object} description of return
+@example
+var code = 'goes here';
+*/
