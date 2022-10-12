0f10cb7138b030542279aa21383d4ffe779ccf1e
Amit Gujarathi <amigujarathi@gmail.com>
2022-10-13 01:23:13 +0530

    Create map.js
    Syntax macro code for javascript MAP function.
    MAP: The map() method creates a new array with the results of calling a function for every array element.

---
+++ General/map.js
@@ -0,0 +1,11 @@
+/**
+* Description : The map() method creates a new array with the results of calling a function for every array element.
+* @orignalArray : Array which needs to be transformed
+* @newArray : Array transformed from orignal array using MAP
+* @return : Transformed Array element						
+*/
+
+var newArray = orignalArray.map(function(val, index){
+	// Business logic to be worked on each element of orignalArray
+	return ;
+})
