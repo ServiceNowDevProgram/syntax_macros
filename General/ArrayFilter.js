945d547cfec3c67ea6b96a234c6a9db99b762274
Amit Gujarathi <amigujarathi@gmail.com>
2022-10-13 01:40:10 +0530

    Create ArrayFilter.js
    Syntax macro for javascript array filter method.
    Filter method: The .filter() method is used to create a new array from a given array consisting of only those elements from the given array which satisfy a condition set by the argument method.

---
+++ General/ArrayFilter.js
@@ -0,0 +1,12 @@
+/**
+* Description : The .filter() method is used to create a new array from a given array consisting of only those elements from the given array which satisfy a condition set by the argument method.
+* @orignalArray : Array which needs to be transformed
+* @newArray : Array transformed from orignal array using filter
+* @return : Transformed Array element						
+*/
+
+var newArray = orignalArray.filter(function(val, index){
+	// Business logic to be worked on each element
+	// If evaluate to true then pushed to newArray else ignored
+	return ;
+})
