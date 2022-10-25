bc864f7abe42374cf9211dcd95b5efa90e1dd6bf
tejudhoran95 <84455070+tejudhoran95@users.noreply.github.com>
2022-10-25 15:09:44 +0530

    Create gsftSubmit.js

---
+++ UI Action/gsftSubmit.js
@@ -0,0 +1,9 @@
+gsftSubmit(null, g_form.getFormElement(), 'action_name');
+
+if (typeof window == 'undefined')
+    serverSideFunction();
+
+/** Server-side function to process the current record and navigate to necessary page**/
+function serverSideFunction() {
+  // server side script here
+}
