51c9a471c3945fd805b8de8b7d0faa4cbddfdaaf
Abhishek Thakur <146176327+at8807602@users.noreply.github.com>
2023-10-02 07:18:00 +0530

    Update Incident Resolver.js
    This resolver is a logic for incident which is defined in GraphQL query schema. It will return the entire record to schema.

--- Integration/GraphQL Schema/Query schema/Incident Resolver.js
+++ Integration/GraphQL Schema/Query schema/Incident Resolver.js
@@ -1,9 +1,9 @@
 (function process(/*ResolverEnvironment*/ env) {
 
-   var user = env.getSource(); // this function will help you to get the sys_id of defined caller id.
+    var sysid = env.getArguments().sys_id; // this function will help you to get the sys_id of defined incident.
 	var now_gr = new GlideRecord('incident');
-	now_gr.addQuery('sys_id',user); // // this will check, if sys_id exist or not.
+	now_gr.addQuery('sys_id',sysid);  // this will check, if sys_id exist or not.
 	now_gr.query();
-	return now_gr;  // This will return the entire record.
+	return now_gr; // This will return the entire record.
 
 })(env);
