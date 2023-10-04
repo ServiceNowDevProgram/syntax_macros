375fa8e4bda5f8fef7b4fc0acee55f264a712921
Deepak Singh <146908519+deepak-64742@users.noreply.github.com>
2023-10-04 14:35:58 +0530

    feat: widget-sharing adding syntax for broadcast
    adding syntax for broadcast values on a specific event from widget

---
+++ Sharing Data Between Widgets/broadcast.js
@@ -0,0 +1,5 @@
+$rootScope.$broadcast('EVENT_NAME', { // EVENT_NAME: string; any name where event needs to trigger 
+  'KEY_1': 'VALUE_1',
+  'KEY_2': 'VALUE_2',
+  //so on... value that will be broadcast on given EVENT_NAME
+});
