f5cf4345e25d3f2ae492a44e8a7710c08b74799b
Deepak Singh <146908519+deepak-64742@users.noreply.github.com>
2023-10-04 14:38:36 +0530

    doc: widget-sharing adding more documentation
    added documentation regarding accessibility of $rootScope in widget controller.

--- Sharing Data Between Widgets/broadcast.js
+++ Sharing Data Between Widgets/broadcast.js
@@ -1,3 +1,4 @@
+// below $rootScope is the controler parameter in widget
 $rootScope.$broadcast('EVENT_NAME', { // EVENT_NAME: string; any name where event needs to trigger 
   'KEY_1': 'VALUE_1',
   'KEY_2': 'VALUE_2',
