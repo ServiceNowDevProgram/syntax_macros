f541aa09b45bb8dcad9bcc2a3baa705d9c448b2f
Deepak Singh <146908519+deepak-64742@users.noreply.github.com>
2023-10-04 12:19:27 +0530

    feat: form-model adding spModal support for syntax macros
    adding support of showing a modal with form using widget, also added a used of form view.

--- spModal/Form Modal.js
+++ spModal/Form Modal.js
@@ -1,1 +1,16 @@
+if (typeof spModal != 'undefined') {
+  	// showing form using form widget
+    spModal.open({
+        title: "MODAL_TITLE",
+        widget: "form", // 'form' is a global widget which is used to show form using table name
+        widgetInput: {
+            table: "TABLE_NAME", // ex. 'incident'
+            view: "FORM_VIEW", // optional parameter, using default view if not using it, ex. 'Major Incidents'
+            sys_id: "RECORD_SYS_ID", // optional parameter to show specific record, ex. '6d744e3997343110f971ffb0f053af6f'
+        },
+        buttons: [] // hide default buttons.
+    });
 
+} else {
+    g_form.addErrorMessage("MESSAGE_HERE");
+}
