cb6ccd2669107ec55e146a8381e2b633c3938956
Sanjiv Meher <sanjiv.meher@yahoo.com>
2023-10-11 14:31:21 -0700

    Update Add User to CC_BCC.js

--- MailScript/Add User to CC/Add User to CC_BCC.js
+++ MailScript/Add User to CC/Add User to CC_BCC.js
@@ -1,4 +1,3 @@
-// Add User to CC add a syntax macro mailcc
-
+// Add User to CC add a code syntax macro mailcc with following code and use in mailscript
 email.addAddress("cc",<email_address>,<name>);
 email.addAddress("bcc",<email_address>,<name>);
