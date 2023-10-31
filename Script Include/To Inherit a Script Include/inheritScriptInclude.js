d2d712a6c600836b08d75e4cbb0b062a195b9366
Shivam Todwal <146908877+shivamTodwal@users.noreply.github.com>
2023-10-31 18:59:45 +0530

    Created a syntax macro to inherit a script include. (#125)

---
+++ Script Include/To Inherit a Script Include/inheritScriptInclude.js
@@ -0,0 +1,12 @@
+var ChildScriptInclude = Class.create();
+ChildScriptInclude.prototype = Object.extendsObject(ParentScriptInclude, {
+    initialize: function() {
+        // Constructor logic for the child script include
+    },
+
+    childFunction: function() {
+        // Child function logic
+    },
+
+    // Override or add more functions as needed
+});
