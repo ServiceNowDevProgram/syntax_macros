8838f2c988893184b3fe2c634ad55534a37e0a44
Ashoo Jindal <94731401+ashoo-jindal@users.noreply.github.com>
2024-10-01 16:28:08 +0530

    Update gr-update-or-insert.js
    Added Example Usage

--- GlideRecord/gr-update-or-insert.js
+++ GlideRecord/gr-update-or-insert.js
@@ -2,6 +2,8 @@
 //             If it does, it updates the designated field; if not, it creates a new record. 
 //             Make sure to modify table_name, field_name, field_to_update, and the associated values as needed.
 
+
+//Code
 var gr = new GlideRecord('table_name');
 gr.addQuery('field_name', 'value');
 gr.query();
@@ -15,6 +17,27 @@ if (gr.next()) {
     // Insert new record
     gr.initialize();
     gr.field_name = 'value';
+    var newRecordID = gr.insert();
+    gs.info('Inserted new record: ' + newRecordID);
+}
+
+
+
+//Example
+var gr = new GlideRecord('incident');
+gr.addQuery('short_description', 'Test GR');
+gr.query();
+
+if (gr.next()) {
+    // Update existing record
+    gr.description = 'Updated Successfully';
+    gr.update();
+    gs.info('Updated record: ' + gr.sys_id);
+} else {
+    // Insert new record
+    gr.initialize();
+    gr.short_description = 'Test GR';
+    gr.description = 'Inserted Successfully';
     var newRecordID = gr.insert();
     gs.info('Inserted new record: ' + newRecordID);
 }
