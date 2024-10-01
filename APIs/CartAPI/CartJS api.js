743b04d7eb43689b7c0aac0db3596a76c9073691
Sachin Singh <152975718+sachinggsingh@users.noreply.github.com>
2024-10-01 12:06:49 +0530

    duplicates have been resolved (#130)
    and a try function is used to see if any error
    occur

--- APIs/CartAPI/CartJS api.js
+++ APIs/CartAPI/CartJS api.js
@@ -2,12 +2,19 @@
 
 var cart = new sn_sc.CartJS(); // CartJS uses a JSON object to capture item and variables
 var item = { 
-	'sysparm_id': 'sysid of catalog item',
+	'sysparm_id': 'actual-sys-id', // replace with actual sys ID
 	'sysparm_quantity': '1',
 	'variables': {
-		'variablename': 'value',
-		'variablename': 'value'
+		'variablename1': 'value', //the duplicates have been resolved here
+		'variablename2': 'value'
 	}
+};
 
-var cartVals = cart.addToCart(item); // this will return a JSON object
-var checkoutVals = cart.checkoutCart(); // this will also return a JSON object containing information about the submitted cart
+try{
+	var cartVals = cart.addToCart(item); // this will return a JSON object
+	var checkoutVals = cart.checkoutCart(); // this will also return a JSON object containing information about the submitted cart
+}
+catch(error)
+{
+	console.log('Error', error);
+}
