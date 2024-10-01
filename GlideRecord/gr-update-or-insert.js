227970a55f24b50d1c1c7eb21576205102e59be2
Ashoo Jindal <94731401+ashoo-jindal@users.noreply.github.com>
2024-10-01 17:56:47 +0530

    GlideRecord (Update Or Insert) (#131)

---
+++ GlideRecord/gr-update-or-insert.js
@@ -0,0 +1,43 @@
+//Description: This macro checks if a record exists based on a specific query. 
+//             If it does, it updates the designated field; if not, it creates a new record. 
+//             Modify table_name, field_name, field_to_update and the associated values as needed.
+
+
+//Code
+var gr = new GlideRecord('table_name');
+gr.addQuery('field_name', 'value');
+gr.query();
+
+if (gr.next()) {
+    // Update existing record
+    gr.field_to_update = 'new_value';
+    gr.update();
+    gs.info('Updated record: ' + gr.sys_id);
+} else {
+    // Insert new record
+    gr.initialize();
+    gr.field_name = 'value';
+    var newRecordID = gr.insert();
+    gs.info('Inserted new record: ' + newRecordID);
+}
+
+
+
+//Example
+var gr = new GlideRecord('incident');
+gr.addQuery('short_description', 'Test GR');
+gr.query();
+
+if (gr.next()) {
+    // Update existing record
+    gr.description = 'Updated Successfully';
+    gr.update();
+    gs.info('Updated record: ' + gr.sys_id);
+} else {
+    // Insert new record
+    gr.initialize();
+    gr.short_description = 'Test GR';
+    gr.description = 'Inserted Successfully';
+    var newRecordID = gr.insert();
+    gs.info('Inserted new record: ' + newRecordID);
+}
