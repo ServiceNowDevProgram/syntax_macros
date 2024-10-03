9a381126213ad4bd83d0cbd260b3a632e207ffe4
aswamy93 <92323937+aswamy93@users.noreply.github.com>
2024-10-03 22:52:04 +0530

    Create syntax editor for the sample code to get local date (#132)
    Generates a sample code to initiate GlideDateTime object and then call the helper function to get the current date as per user's time-zone

---
+++ GlideDateTime/getlocaldate.js
@@ -0,0 +1,2 @@
+var gdt = new GlideDateTime();//Initiate the date time object for the current date & time
+gdt.getLocalDate();//Gets the date stored by the date time object, expressed in the standard format, yyyy-MM-dd, and the current user's time zone
