44b298060cb005af6be0169bfe47ab6d6bd87463
JAHNAVI THONDEPU <95975610+jahnaviT2003@users.noreply.github.com>
2024-10-16 15:04:44 +0530

    Create beginEndDay.js

---
+++ Date Time/Date and Time Syntax Macros/beginEndDay.js
@@ -0,0 +1,4 @@
+var dateTime = new GlideDateTime();
+dateTime.setDisplayValue("2024-10-16");
+var startOfDay = dateTime.getStartOfDay();  // Get the start of the day
+var endOfDay = dateTime.getEndOfDay();  // Get the end of the day
